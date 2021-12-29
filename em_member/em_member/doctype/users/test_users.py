9daccd608f2d54e99b10c2d10dbabc3f83fced8e
Gedewon <gedewon.hayle.9@gmail.com>
2021-12-29 19:33:34 +0300

    creating bear bone Doctypes

---
+++ em_member/em_member/doctype/users/test_users.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, 360ground and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestUsers(unittest.TestCase):
+	pass
---
+++ em_member/em_member/doctype/users/users.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, 360ground and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Users', {
+	// refresh: function(frm) {
+
+	// }
+});
