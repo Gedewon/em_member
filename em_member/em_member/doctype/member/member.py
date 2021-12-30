7f5fdde17c56c1b7a6f1ed81924fefa1ec858666
Gedewon <gedewon.hayle.9@gmail.com>
2021-12-30 11:32:17 +0300

    make educational credentials multiple field

--- em_member/em_member/doctype/member/member.py
+++ em_member/em_member/doctype/member/member.py
@@ -3,10 +3,22 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 class Member(Document):
 	def before_save(self):
-		print("--------------------------")
+		# before_insert do
+		# check the type of membership field choosen 
+		# add to specific group and to the group ALL
+		# add_subsrcibers(name,email_list)
+		# name -> type of emailgroup
+		# email_list -> have to be array(list,tuple)
+		# membership_type = self.membership_type
+		# email = self.email
+		# abc = frappe.get_doc('Email group',membership_type)
+		# # how do we consume a whitelist functions
+		# add_subscribers("ALL",email)
+		# frappe.email_group.add_subscribers(membership_type,email)
+		# print("--------------------------")
 
 		print(self)
 		print(self.membership_type)
